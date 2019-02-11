968525a4363e1056c6c0013e3a7911ab42b7cbfb
Tiago Alves <alvesjtiago@gmail.com>
2019-02-11 19:28:03 +0000

    Search by cache key.

--- app/controllers/fastentry/dashboard_controller.rb
+++ app/controllers/fastentry/dashboard_controller.rb
@@ -4,13 +4,17 @@ module Fastentry
       @keys = Rails.cache.instance_variable_get(:@data).keys
 
       @cached = []
-      @keys.first(20).each do |key|
+      @keys.each do |key|
         expiration_date = Rails.cache.send(:read_entry, key, {}).expires_at
         @cached << {
           cache_key: key,
           cache_value: Rails.cache.read(key.to_s),
           expiration: (Time.at(expiration_date) if expiration_date.present?)
         }
+      end
+
+      if params[:query].present?
+        @cached = @cached.select { |c| c[:cache_key].include?(params[:query]) }
       end
     end
   end
