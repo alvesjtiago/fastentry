7603fcbd59a9abae6fd3bdfb2166ffa8a1048370
Tiago Alves <alvesjtiago@gmail.com>
2019-02-11 20:19:03 +0000

    First pagination implementation (WIP)

--- app/controllers/fastentry/dashboard_controller.rb
+++ app/controllers/fastentry/dashboard_controller.rb
@@ -1,7 +1,17 @@
 module Fastentry
   class DashboardController < ApplicationController
+    before_action :set_page, only: [:index]
+
     def index
       @keys = Rails.cache.instance_variable_get(:@data).keys
+
+      if params[:query].present?
+        @keys = @keys.select { |c| c.include?(params[:query]) }
+      end
+
+      @number_of_pages = (@keys.count / @per_page.to_f).ceil
+
+      @keys = @keys[@offset, @per_page] || []
 
       @cached = []
       @keys.each do |key|
@@ -12,10 +22,14 @@ module Fastentry
           expiration: (Time.at(expiration_date) if expiration_date.present?)
         }
       end
+    end
 
-      if params[:query].present?
-        @cached = @cached.select { |c| c[:cache_key].include?(params[:query]) }
-      end
+    private
+
+    def set_page
+      @page = params[:page].to_i || 0
+      @per_page = 20
+      @offset = @page * @per_page
     end
   end
 end
