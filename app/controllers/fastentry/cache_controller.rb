7e50d18e1317cd095f34a90f749c19e5d1b968dc
Tiago Alves <alvesjtiago@gmail.com>
2019-02-12 19:18:51 +0000

    Add more information to cache item view

--- app/controllers/fastentry/cache_controller.rb
+++ app/controllers/fastentry/cache_controller.rb
@@ -1,8 +1,10 @@
 module Fastentry
   class CacheController < ApplicationController
     def show
-      key = params[:key]
-      @cache_item = Rails.cache.read(key)
+      @key = params[:key]
+      expiration_date = Rails.cache.send(:read_entry, @key, {}).expires_at
+      @expiration = (Time.at(expiration_date).strftime("%a, %e %b %Y %H:%M:%S %z") if expiration_date.present?)
+      @cache_item = Rails.cache.read(@key)
     end
 
     def invalidate
