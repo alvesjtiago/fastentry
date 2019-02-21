dab1339fe31eb19646d31c47ea019fc2be2e2263
Kevin Deisz <kevin.deisz@gmail.com>
2019-02-21 09:38:33 -0500

    Support multiple caches
    Refactor to use Fastentry.cache instead of Rails.cache which decorates with support for pulling out keys in different ways.

--- app/controllers/fastentry/cache_controller.rb
+++ app/controllers/fastentry/cache_controller.rb
@@ -2,20 +2,16 @@ module Fastentry
   class CacheController < Fastentry::ApplicationController
     def show
       @key = params[:key]
-      
-      begin
-        expiration_date = Rails.cache.send(:read_entry, key, {}).expires_at
-      rescue
-        expiration_date = nil
-      end
 
-      @expiration = (Time.at(expiration_date).strftime("%a, %e %b %Y %H:%M:%S %z") if expiration_date.present?)
-      @cache_item = Rails.cache.read(@key)
+      expiration = Fastentry.cache.expiration_for(key)
+      @expiration = expiration.strftime("%a, %e %b %Y %H:%M:%S %z") if expiration
+
+      @cache_item = Fastentry.cache.read(@key)
     end
 
     def invalidate
       key = params[:key]
-      Rails.cache.delete(key)
+      Fastentry.cache.delete(key)
 
       redirect_back(fallback_location: root_path)
     end
--- lib/fastentry.rb
+++ lib/fastentry.rb
@@ -1,6 +1,48 @@
-require "fastentry/engine"
+require 'fastentry/engine'
 require 'awesome_print'
 
 module Fastentry
-  # Your code goes here...
+  class Cache < SimpleDelegator
+    alias cache __getobj__
+
+    def expiration_for(key)
+      expires_at = cache.send(:read_entry, key, {}).expires_at
+      expires_at.strip.empty? ? nil : Time.at(expires_at)
+    rescue StandardError
+      nil
+    end
+
+    def read(key)
+      cache.read(key)
+    rescue StandardError
+      nil
+    end
+
+    def self.for(cache)
+      case cache
+      when ActiveSupport::Cache::Strategy::LocalCache
+        LocalCache.new(cache)
+      when ActiveSupport::Cache::RedisCacheStore
+        RedisCache.new(cache)
+      else
+        raise ArgumentError, 'Unsupported cache type'
+      end
+    end
+  end
+
+  class LocalCache < Cache
+    def keys
+      cache.instance_variable_get(:@data).keys
+    end
+  end
+
+  class RedisCache < Cache
+    def keys
+      cache.redis.keys
+    end
+  end
+
+  def self.cache
+    @cache ||= Cache.for(Rails.cache)
+  end
 end
