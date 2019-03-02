1607b9cbffffc09dd54c3fd3ae3a06df28f21b10
Tiago Alves <alvesjtiago@gmail.com>
2019-03-02 18:45:22 +0000

    Test cache number of keys

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -20,7 +20,14 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+
+  # Perform cache
+  config.action_controller.perform_caching = true
+
+  config.cache_store = :memory_store
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
--- spec/lib/fastentry_spec.rb
+++ spec/lib/fastentry_spec.rb
@@ -4,4 +4,9 @@ describe Fastentry do
   it "should return a default Fastentry cache instance" do
     expect(Fastentry.cache).to be_a_kind_of(Fastentry::DefaultCache)
   end
+
+  it "should return the correct number of cached keys" do
+    Rails.cache.write("city", "Porto")
+    expect(Fastentry.cache.keys.size).to eq(1)
+  end
 end
